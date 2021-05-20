8b1a957322964ab015a5f8f9a131b4fa6d48118b
bonarhyme <bonarhyme@gmail.com>
2021-05-20 13:43:48 +0100

    Completed the ui for navbar

--- index.js
+++ index.js
@@ -1,15 +1,55 @@
+const thumbnail = document.querySelector(".thumbnail");
+const named = document.querySelector(".name");
+const navToggle = document.querySelector(".nav");
+const buttonToggle = document.querySelector("button.bars");
+
 const github_data = {
-  token: "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",
-  username: "bonarhyme",
+  token: "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",
 };
 
 const body = {
   query: `
-    query { 
-        user(login: ${github_data.username}){
-          avatarUrl
+  query { 
+    user(login: "bonarhyme"){
+    avatarUrl
+    bioHTML
+    login
+    name
+    websiteUrl
+    twitterUsername
+    location
+        followers{
+          totalCount
         }
+        following{
+          totalCount
+        }
+        status{
+          emojiHTML
+          message
+        }
+    repositories(first: 20, privacy: PUBLIC, affiliations: OWNER, ownerAffiliations: OWNER, isFork: false){
+      nodes{
+        name
+        url
+        description
+        languages(last: 10){
+          nodes{
+            name
+            color
+            id
+          }
+        }
+        homepageUrl
+        forkCount
+        pullRequests(last: 10){
+          totalCount
+        }
+        updatedAt
       }
+    }
+    }
+}
     `,
 };
 
@@ -17,21 +57,29 @@ const baseUrl = "https://api.github.com/graphql";
 
 const headers = {
   "Content-Type": "application/json",
-  Authentication: "bearer " + github_data.token,
+  Authorization: "bearer " + github_data.token,
 };
 
 async function postData(url = "", body) {
-  const response = await fetch(url, {
+  fetch(url, {
     method: "POST",
-
+    referrerPolicy: "origin-when-cross-origin",
     headers,
     body: JSON.stringify(body),
-  });
-  return response.json();
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      thumbnail.src = data.data.user.avatarUrl;
+      named.innerText = data.data.user.login;
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+    });
 }
 
-postData(baseUrl, body)
-  .then((response) => {
-    console.log(response);
-  })
-  .catch((error) => console.log(JSON.stringify(error)));
+postData(baseUrl, body);
+
+buttonToggle.addEventListener("click", () => {
+  navToggle.classList.toggle("toggle");
+});
